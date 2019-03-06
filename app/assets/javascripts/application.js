18372f21702b750d337fcc74d2cfb0c50d221fcf
Angelo Spampinato <spampinatodesign@gmail.com>
2019-03-06 18:35:17 -0500

    Lizzie got the progress bar working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,26 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery3
+//= require jquery_ujs
 //= require_tree .
+
+
+
+let songId;
+// This code enables to have access to the particular song's ID
+document.addEventListener('DOMContentLoaded',()=>{
+	document.querySelector('ul').addEventListener('click', (event)=>{
+		if (event.target.tagName == 'BUTTON') {
+			// debugger
+			songId = event.target.dataset.id
+			console.log(songId)
+		}
+	})
+})
+// This function is for the progress bar
+	function updateProgress() {
+	    var player = document.getElementById(songId);
+	    var progressbar = document.querySelector(`#bar-${songId}`);
+	    progressbar.value = (player.currentTime / player.duration);
+	};
