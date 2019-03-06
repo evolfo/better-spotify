4a57a5a582d01b496dd93d298be92422925f679c
Angelo Spampinato <spampinatodesign@gmail.com>
2019-03-06 16:13:50 -0500

    song r werking

--- lib/assets/song_creation.rb
+++ lib/assets/song_creation.rb
@@ -2,7 +2,7 @@ def get_songs_from_folders
   music = {}
   ##### FILE PATH #####
   ##### "../app/assets/songs" #####
-  Dir.foreach("./app/assets/songs") do |folder|
+  Dir.foreach("./app/assets/audios") do |folder|
     next if folder == '.' or folder == '..' or folder == ".DS_Store"
 
     def delete_extension(file_name)
@@ -12,10 +12,11 @@ def get_songs_from_folders
     music[folder] = {}
     ##### FILE PATH #####
     ##### "../app/assets/songs/#{folder}" #####
-    Dir.foreach("./app/assets/songs/#{folder}") do |song|
+    Dir.foreach("./app/assets/audios/#{folder}") do |song|
       next if song == '.' or song == '..' or folder == ".DS_Store"
       ##### FILE PATH #####
-      music[folder][delete_extension(song)] = "./app/assets/songs/#{folder}/#{song}"
+      i = 0
+      music[folder][delete_extension(song)] = i += 1
     end
   end
   music
@@ -24,8 +25,8 @@ end
 def create_songs(hash)
   hash.each do |artist_name, data|
     artist = Artist.find_by(name: artist_name)
-    data.each do |song_name, song_path|
-      Song.create(artist_id: artist.id, title: song_name, path: song_path)
+    data.each do |song_name, song_number|
+      Song.create(artist_id: artist.id, title: song_name)
     end
   end
 end
